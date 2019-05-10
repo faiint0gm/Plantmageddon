bab85ae6c543ef408a033d8604b86fba56592965
Piotrek Rudnicki <p.rudnicki@developerodzera.pl>
2019-05-10 15:17:08 +0200

    Some JSON add to datamanager

--- Assets/Scripts/DataManagement/DataManager.cs
+++ Assets/Scripts/DataManagement/DataManager.cs
@@ -1,11 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class DataManager : MonoBehaviour
 {
+    private DataManager _instance;
+    public DataManager Instance
+    {
+        get {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("DataManager");
+                go.AddComponent<DataManager>();
+                _instance = go.GetComponent<DataManager>();
+            }
+            return _instance;
+        }        
+    }
+
     private void Start()
     {
         DontDestroyOnLoad(this);
+    }
+
+    public static void SaveJSONData()
+    {
+
+    }
+}
+
+    public static void LoadJSONData()
+    {
+        string loadPath = "./Assets/Resources/JsonData/Data.json";
+        using (StreamReader reader = new StreamReader(loadPath))
+        {
+            string json = reader.ReadToEnd();
+
+            //data = JsonHelper.getJsonArray<HumanUnit>(json);
+        }
     }
 }
